4e2ba3af89f9a140eeb2bf8c8ba2f7bafb10d6af
Dat <kaka391989@gmail.com>
2018-09-22 11:58:52 +0700

    Backup for add OData in backend.

--- src/EcommerceApp/src/app/services/categories.service.ts
+++ src/EcommerceApp/src/app/services/categories.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-
 import { HttpHeaders } from '@angular/common/http';
-
-
 import { Observable, BehaviorSubject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
 import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
-
+import { environment } from '../../environments/environment';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
@@ -20,7 +16,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class CategoriesService {
-  categoriesUrl = 'http://localhost:59362/api/categories';  // URL to web api
+  categoriesUrl = environment.ApiUrl + '/api/categories';  // URL to web api
   private handleError: HandleError;
 
   dataChange: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
